d5aff5e51b427c6abd09dd4693f2cd46e52bb48c
rifat-sultana <rifatsultana88.web@gmail.com>
2026-03-29 23:30:45 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+
+<h2 className='bg-amber-400 text-blue-700 '> hello </h2>
+      
+      <div>
+        <div className="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">Card Title</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+      </div>
+
+
+
+    </>
+  )
+}
+
+export default App
