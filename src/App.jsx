a29631a617965896df160fda528ed35d316adce3
rifat-sultana <rifatsultana88.web@gmail.com>
2026-03-30 20:32:01 +0600

    feat: add hero section including title, description, and buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 
 import './App.css'
+import Herosection from './Components/Herosection/Herosection'
 import Navbar from "./Components/Navbar"
+
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -8,6 +10,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Herosection />
 
 
 
